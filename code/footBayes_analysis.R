09276c35eb393781d2992f62c14f725f50ae5c40
Giulio Fantuzzi <74678041+giuliofantuzzi@users.noreply.github.com>
2023-03-14 10:49:02 +0100

    Add files via upload

--- code/footBayes_analysis.R
+++ code/footBayes_analysis.R
@@ -1,7 +1,7 @@
 #_______________________________________________________________________________         
 #        Author:    Giulio Fantuzzi
 #       Created:    2023/03/09
-# Last modified:    2023/03/09
+# Last modified:    2023/03/10
 #         About:    Analysis by using "FootBayes" package
 #_______________________________________________________________________________         
 
@@ -12,25 +12,24 @@ library(footBayes)
 library(engsoccerdata)
 library(tidyverse)
 
-# Import serieA 2021/2022 data
-data(italy)
-italy <- as.data.frame(italy)
-italy_2010 <- subset(italy[, c(2,3,4,6,7)],
-                     Season =="2010")
 #-------------------------------------------------------------------------------
+# Import 2020/2021 data
+serieA_2122<- read.csv("../data/serieA_21-22.csv")
+# Feature selection
+serieA_2122<- serieA_2122[,c("HomeTeam", "AwayTeam", "FTHG", "FTAG")]
+# Change dataframe format to apply footBayes package
+serieA_2122<- data.frame(cbind(rep(2010,380),serieA_2122))
+colnames(serieA_2122)= c("Season", "home", "visitor", "hgoal", "vgoal")
+#-------------------------------------------------------------------------------
+fit_mle<- mle_foot(data = serieA_2122,
+                   model="biv_pois",
+                   interval = "Wald")
 
-# Fit model by maximum likelihood estimation
-fit1_mle <- mle_foot(data = italy_2010,
-                     model="biv_pois",
-                     interval = "Wald") # mle biv poisson
-
-#Plot team's coefficients
-foot_abilities(object = fit1_mle, data = italy_2010, cex.var = 1)
-
-# Dynamic approach-->coefficients change over time
-fit1_stan <- stan_foot(data = italy_2010,
-                       model="biv_pois",
-                       dynamic_type ="weekly",
-                       iter=40)
-
-foot_abilities(fit1_stan, italy_2010)
+#Plot teams coefficients
+foot_abilities(object = fit_mle, data = serieA_2122,type = c("attack"))
+foot_abilities(object = fit_mle, data = serieA_2122,type = c("defense"))
+#attack parameters (strenght in attack)
+sort(fit_mle$att[,2], decreasing = T)
+#defense parameters (team weakness in defense)
+sort(fit_mle$def[,2], decreasing = F)
+#-------------------------------------------------------------------------------
--- code/home_effect.R
+++ code/home_effect.R
@@ -61,7 +61,93 @@ ggplot(goalconceeded_df, aes(x=Teams, y=Goals, fill=Stadium)) +
     theme(plot.title = element_text(hjust = 0.5,face="bold"))
 # 11 teams conceeded more Away, 6 conceeded more Home; 1 conceeded equally
 #-------------------------------------------------------------------------------
-#Another way to show this is plot goal difference Home - Away
-#-------------------------------------------------------------------------------
 #---> There's an evident home effect, and it has a bigger impact on the goal scored
 #-------------------------------------------------------------------------------
+
+#Another way to show this is plot goal difference Home - Away
+
+#-------------------------------------------------------------------------------
+# Dataframes
+goal_scored_difference<- TeamHomeGoalsScored- TeamAwayGoalsScored
+# Creazione di un dataframe di esempio
+goal_scored_difference_df <- data.frame(
+    teams_list,
+    goal_scored_difference
+)
+goal_conceeded_difference<- TeamHomeGoalsConceeded- TeamAwayGoalsConceeded
+# Creazione di un dataframe di esempio
+goal_conceeded_difference_df <- data.frame(
+    teams_list,
+    goal_conceeded_difference
+)
+# Creazione della variabile per il colore
+goal_scored_difference_df$colore <- ifelse(goal_scored_difference_df$goal_scored_difference >= 0, 
+                                           "#52BE80", "#EC7063")
+goal_conceeded_difference_df$colore <- ifelse(goal_conceeded_difference_df$goal_conceeded_difference >= 0, 
+                                              "#EC7063", "#52BE80")
+#-------------------------------------------------------------------------------
+# Plots
+# Goal scored difference Home vs Away
+ggplot(goal_scored_difference_df, aes(x = teams_list, y = goal_scored_difference, fill = colore)) +
+    geom_bar(stat = "identity", width=0.6, position=position_dodge(),colour="black") +
+    scale_fill_identity() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,face="bold"))+
+    labs(title = "Goal scored difference Home vs Away",
+         x = "", 
+         y = "Goal scored difference")+
+    theme(plot.title = element_text(hjust = 0.5,face="bold"))
+
+# Goal conceeded difference Home vs Away
+ggplot(goal_conceeded_difference_df, aes(x = teams_list, y = goal_conceeded_difference, fill = colore)) +
+    geom_bar(stat = "identity", width=0.6, position=position_dodge(),colour="black") +
+    scale_fill_identity() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,face="bold"))+
+    labs(title = "Goal conceeded difference Home vs Away",
+         x = "", 
+         y = "Goal conceeded difference")+
+    theme(plot.title = element_text(hjust = 0.5,face="bold"))
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+# Analisi dei risultati utili in casa vs fuori casa
+serieA_2122$useful<-  (serieA_2122$FTR=="H") | (serieA_2122$FTR=="D")
+useful_results_home<- vector(mode="numeric", length=length(teams_list))
+useful_results_away<- vector(mode="numeric", length=length(teams_list))
+for (i in 1:length(teams_list)){
+    useful_results_home[i]<- sum(serieA_2122[serieA_2122$HomeTeam== teams_list[i],"useful"])
+    useful_results_away[i]<- sum(!serieA_2122[serieA_2122$AwayTeam== teams_list[i],"useful"])
+}
+
+useful_results_df <- data.frame(Stadium=rep(c("Home", "Away"),each=20),
+                            Teams=rep(teams_list,2),
+                            Result=c(useful_results_home,useful_results_away)
+)
+
+ggplot(useful_results_df, aes(x=Teams, y=Result, fill=Stadium)) + 
+    geom_bar(stat="identity", width=0.6, position=position_dodge(),colour="black")+
+    scale_fill_manual(values = c("#EC7063", "#52BE80"))+
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,face="bold"))+
+    ggtitle("USEFUL RESULTS SERIE A 21-22") +
+    theme(plot.title = element_text(hjust = 0.5,face="bold"))
+
+#Oppure con la differenza
+
+useful_result_difference<- useful_results_home- useful_results_away
+# Creazione di un dataframe di esempio
+useful_result_difference_df <- data.frame(
+    teams_list,
+    useful_result_difference
+)
+
+useful_result_difference_df$Better<- ifelse(useful_result_difference_df$useful_result_difference > 0, 
+                                           "Home", "Away")
+
+#Useful result difference Home vs Away
+ggplot(useful_result_difference_df, aes(x = teams_list, y = useful_result_difference, fill = Better)) +
+    geom_bar(stat = "identity", width=0.6, position=position_dodge(),colour="black") +
+    scale_fill_manual(values = c("#EC7063", "#52BE80"))+
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,face="bold"))+
+    labs(title = "Useful result difference Home vs Away",
+         x = "", 
+         y = "Useful result difference")+
+    theme(plot.title = element_text(hjust = 0.5,face="bold"))
